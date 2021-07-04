7c6a6dd8eb819d44d1b621790105c353e6ba3c65
sic46 <66070130+sic46@users.noreply.github.com>
2021-07-05 00:01:13 +1000

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,8 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
+
 
 import Header from "./header"
 import "./layout.css"
