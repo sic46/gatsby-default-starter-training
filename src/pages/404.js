82b5b2103fd9a3ba7e5d387da6e0c63d1266c223
sic46 <66070130+sic46@users.noreply.github.com>
2021-07-05 00:04:11 +1000

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,13 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>Whoops, we're lost. Let's try again.
-      <Link to="/"Go home </Link>
+      <Link to="/">Go home </Link>
       </p>
   </Layout>
 )
