46ff521f190e0e55ddf58a6698b8e42375b2203c
sic46 <66070130+sic46@users.noreply.github.com>
2021-07-06 00:06:26 +1000

    Revert "Update header.js"
    This reverts commit 484d23528489d072d416d167ca6f0d357f2fa32e.

--- src/components/header.js
+++ src/components/header.js
@@ -9,24 +9,13 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
-    <nav className="ds-global-nav">
-      <div className="ds-global-nav__wrapper">
-      <div className="ds-global-home-link">
-      <Link to="/"> <StaticImage src="../images/tplr_e22.png" alt="The Pit Lane Report" height={"80"} width={"239"}/></Link>
-      </div>
-      <ul className="ds-global-nav__child ds-global-nav__child--selected">
-      <li class="ds-global-nav__child ds-global-nav__child--selected"><a href="">Study with us</a></li>
-                <li class="ds-global-nav__child"><a href="">Team</a></li>
-                <li class="ds-global-nav__child"><a href="">Car</a></li>
-                <li class="ds-global-nav__child"><a href="">Championship</a></li>
-                <li class="ds-global-nav__child"><a href="">Fans</a></li>
-                <li class="ds-global-nav__child"><a href="">Merchandise</a></li>
-      </ul>
-    style={{
+    <div
+      style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
+    >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -38,10 +27,7 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-      </div></nav>
-    
-            
-      
+    </div>
   </header>
 )
 
