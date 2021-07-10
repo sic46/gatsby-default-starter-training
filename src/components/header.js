b38d850db288bde660a77af722e53af5f10dcfd8
sic46 <66070130+sic46@users.noreply.github.com>
2021-07-11 00:42:01 +1000

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -19,10 +19,6 @@ const Header = ({ siteTitle }) => (
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
         >
           {siteTitle}
         </Link>
